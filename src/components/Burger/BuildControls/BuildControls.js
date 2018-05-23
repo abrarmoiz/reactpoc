f6e47bf1a5c3666da3a6233cd007249a8d2efad6
abrarmoiz <abrar.moiz@gmail.com>
2018-05-24 00:29:17 +0530

    init comment

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import classes from './BuildControl.css'
+
+const buildControls = (props) =>{ }
+export default buildControls;
---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import classes from './Burger.css';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+const burger = (props) => {
+    
+    //We are transforming Ingredients in form of cheese : 3 => array of cheese, cheese, cheese 
+    let transformedIngredients = Object.keys(props.ingredients)
+        .map(igKey  => { return [...Array(props.ingredients[igKey])] //internal map
+                .map((_,i) =>{
+                   return <BurgerIngredient key={igKey + i} type={igKey}/>;
+                });
+            }).reduce((arr, el) => {return arr.concat(el);}, []);
+        
+        if(transformedIngredients.length == 0){
+            transformedIngredients= <p>Please add ingredients!!!</p>
+        }
+
+        console.log(transformedIngredients);
+    return (   
+            <div className={classes.Burger}>
+                <BurgerIngredient type="bread-top"/>
+                {transformedIngredients}
+                <BurgerIngredient type="bread-bottom"/>
+            </div>
+            );
+
+}
+
+export default burger;
