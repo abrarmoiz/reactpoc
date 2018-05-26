3ae23410eb5157af16f0c0f83949b922cb480cc4
abrarmoiz <abrar.moiz@gmail.com>
2018-05-26 23:13:02 +0530

    updated controls

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,5 +1,33 @@
-import React from 'react';
-import classes from './BuildControl.css'
+import React, {Component} from 'react';
+import classes from './BuildControls.css'
+import BuildControl from './BuildControl/BuildControl'
 
-const buildControls = (props) =>{ }
-export default buildControls;
+const controls = [
+    {label: 'Salad', type: 'salad'},
+    {label: 'Bacon', type: 'bacon'},
+    {label: 'Cheese', type: 'cheese'},
+    {label: 'Meat', type: 'meat'}
+]; 
+
+class BuildControls extends Component {
+    render(){
+      return   (
+               
+                <div className={classes.BuildControls}> 
+                <p> Total Price {this.props.price.toFixed(2)} </p>
+                    {controls.map(ctrl => ( 
+                        <BuildControl 
+                            key={ctrl.label} 
+                            label={ctrl.label} 
+                            ingredientAdded={() => this.props.ingredientAdded(ctrl.type)}
+                            ingredientRemoved={() => this.props.ingredientRemoved(ctrl.type)}
+
+                            
+                        /> 
+                    ))}
+                </div>);
+            };
+
+}
+
+export default BuildControls;
